2a3416fce24a09d1ce2f2ca897fd541cedcaed81
unknown <sebasgiraldo1003@gmail.com>
2024-03-19 14:59:18 -0500

    base de datos creada

--- corp/models.py
+++ corp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
+class Empresa(models.Model):
+    id = models.IntegerField(primary_key=True, validators=[MinValueValidator(1),MaxValueValidator(999_999_999)], unique=True)
+    nombre = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=250)
+    tipo = models.CharField(max_length=50, default='N/A')
+    ubicacion = models.CharField(max_length=100, default = 'N/A')
+    salario = models.CharField(max_length = 10, default = '0')
+    nivel_marca_empleadora = models.CharField(max_length = 50, default = 'Marca empleadora estandar')
+    calificacion = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    imagen = models.ImageField(upload_to='corp/images/', default='corp/images/default.png')
+    redes = models.TextField(default = 'N/A')
