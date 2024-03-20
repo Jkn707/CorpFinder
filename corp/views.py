897e085aa53c9af5b775cfdc8f56476d9bab886f
unknown <sebasgiraldo1003@gmail.com>
2024-03-20 12:20:03 -0500

    visualizar y buscar empresas

--- corp/views.py
+++ corp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Empresa
+
 # Create your views here.
+
 def home(request):
    # return HttpResponse('<h1> Bienvenido a CorpFinder </h1>')
    # return render(request, 'home.html')
-    return render(request, 'home.html',{'name': 'Paola Noreña'})
+   #  return render(request, 'home.html',{'name': 'Paola Noreña'})
+   searchTerm = request.GET.get('buscarEmpresa')
+   if searchTerm:
+      empresas = Empresa.objects.filter(nombre__icontains=searchTerm)
+   else:
+      empresas = Empresa.objects.all()
+   return render(request,'home.html',{'searchTerm':searchTerm, 'empresas':empresas})
